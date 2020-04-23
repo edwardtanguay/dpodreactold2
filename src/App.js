6bf8f32bc20e3f68e3c0a45497f7ec479f54fa26
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-04-23 21:45:02 +0200

    added version info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <p>Welcome to Datapod for React version 0.00.01.</p>
+            <p>Welcome to Datapod for React.</p>
+            <ul>
+                <li>0.00.01 initial site</li>
+                <li>0.00.02 added version info</li>
+            </ul>
         </div>
     );
 }
