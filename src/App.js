74a93bf260691bbc010a3dc06daf786549da6dd6
Edward Tanguay (wk.win) <edward.tanguay@gefegmbh.local>
2020-04-27 16:45:01 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <p>Welcome to Datapod for React.</p>
-            <ul>
-                <li>0.0.0.003 fixed versions to be more standard n.n.n</li>
-                <li>0.0.0.002 added version info</li>
-                <li>0.0.0.001 initial site</li>
-            </ul>
-        </div>
-    );
+	return (
+		<div className="App">
+			<p>Welcome to Datapod for React.</p>
+			<ul>
+				<li>0.0.0.003 fixed versions to be more standard n.n.n</li>
+				<li>0.0.0.002 added version info</li>
+				<li>0.0.0.001 initial site</li>
+				<li>initial state</li>
+			</ul>
+		</div>
+	);
+
 }
 
 export default App;
