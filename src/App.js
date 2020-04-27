8b71b0321acfc98c6c9a658e047a45dedee43069
Edward Tanguay (wk.win) <edward.tanguay@gefegmbh.local>
2020-04-27 17:36:02 +0200

    fifth version

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 		<div className="App">
 			<p>Welcome to Datapod for React.</p>
 			<ul>
+				<li>0.0.0.005 second five</li>
 				<li>0.0.0.004 fourth change</li>
 				<li>0.0.0.003 fixed versions to be more standard n.n.n</li>
 				<li>0.0.0.002 added version info</li>
