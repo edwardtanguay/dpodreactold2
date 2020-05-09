32de9e4d66f09db845fe17065a52a6fe8d272514
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-09 16:34:48 +0200

    menu with icons

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaListAlt } from 'react-icons/fa';
+import { BsArrowLeftRight, BsInfoSquare } from "react-icons/bs";
+import { MdCreate } from "react-icons/md";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
 
 function App() {
 	return (
 		<div className="container mt-4">
-			<ul class="nav nav-pills flex-column flex-md-row">
+			<ul class="nav nav-pills">
 				<li className="nav-item"><a href="#" className="nav-link disabled font-weight-bold">Datapod for React</a></li>
-				<li className="nav-item"><a href="#" className="nav-link active">Info</a></li>
-				<li className="nav-item"><a href="#" className="nav-link">Create Item</a></li>
-				<li className="nav-item"><a href="#" className="nav-link">Create Page</a></li>
-				<li className="nav-item"><a href="#" className="nav-link">About</a></li>
+				<li className="nav-item"><a href="#" className="nav-link"><BsInfoSquare/> Info</a></li>
+				<li className="nav-item"><a href="#" className="nav-link active"><BsArrowLeftRight/> Create Item</a></li>
+				<li className="nav-item"><a href="#" className="nav-link"><MdCreate/> Create Page</a></li>
+				<li className="nav-item"><a href="#" className="nav-link"><AiOutlineQuestionCircle/> About</a></li>
 			</ul>
 
 			<article>
