6742af1487b47b707faf7a7ad1441e8b759ef92f
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-09 22:49:49 +0200

    fixed menu

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ import { BsArrowLeftRight, BsInfoSquare } from "react-icons/bs";
 import { MdCreate } from "react-icons/md";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 import Item from './components/Item';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, MemoryRouter } from 'react-router-dom';
 import PageCreatePage from './components/PageCreatePage';
 import PageWelcome from './components/PageWelcome';
 import PageCreateItem from './components/PageCreateItem';
 import PageAbout from './components/PageAbout';
+import { NavLink } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="container mt-4">
-			<ul class="nav nav-pills text-left">
-				<li className="nav-item"><a href="" className="nav-link disabled font-weight-bold">Datapod for React</a></li>
-				<li className="nav-item"><a href="/welcome" className="nav-link"><BsInfoSquare /> Welcome</a></li>
-				<li className="nav-item"><a href="/createItem" className="nav-link"><BsArrowLeftRight /> Create Item</a></li>
-				<li className="nav-item"><a href="/createPage" className="nav-link"><MdCreate /> Create Page</a></li>
-				<li className="nav-item"><a href="/about" className="nav-link"><AiOutlineQuestionCircle /> About</a></li>
-			</ul>
+			<BrowserRouter>
+				<ul class="nav nav-pills text-left">
+					<li className="nav-item"><NavLink to="/welcome" className="nav-link"><BsInfoSquare /> Welcome</NavLink></li>
+					<li className="nav-item"><NavLink to="/createItem" className="nav-link"><BsArrowLeftRight /> Create Item</NavLink></li>
+					<li className="nav-item"><NavLink to="/createPage" className="nav-link"><MdCreate /> Create Page</NavLink></li>
+					<li className="nav-item"><NavLink to="/about" className="nav-link"><AiOutlineQuestionCircle /> About</NavLink></li>
+				</ul>
 
-			<div class="ml-4">
-				<BrowserRouter>
+				<div class="ml-4">
 					<Switch>
 						<Route path="/welcome" component={PageWelcome} />
 						<Route path="/createItem" component={PageCreateItem} />
@@ -32,8 +32,8 @@ function App() {
 						<Route path="/about" component={PageAbout} />
 						<Route path="/" component={Item} />
 					</Switch>
-				</BrowserRouter>
-			</div>
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 }
