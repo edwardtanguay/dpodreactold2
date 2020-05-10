e9f3fe3e6b3f508bd2b541e1f2a4ef468e5bc3a0
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-10 18:10:54 +0200

    with flashcards

--- src/App.js
+++ src/App.js
@@ -15,21 +15,39 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			location: "Berlin"
+			flashcards: []
 		}
 	}
+	componentDidMount() {
+		fetch('./flashcards.json')
+			.then(response => response.json())
+			.then(result => {
+				const f = result.map(item => {
+					return item;
+				});
+				this.setState({
+					flashcards: f
+				})
+			})
+	}
 	render() {
+		const flashcardsHtml = this.state.flashcards.map(item => (
+			<div className="mb-2">
+				<div className="bg-secondary p-1 text-white font-italic">{item.front}</div>
+				<div className="p-1 theCode">{item.back}</div>
+			</div>
+		));
 		return (
 			<main className="container mt-4">
 				<BrowserRouter>
-					<ul class="nav nav-pills text-left pl-4 pb-3">
+					<ul className="nav nav-pills text-left pl-4 pb-3">
 						<li className="nav-item"><NavLink to="/welcome" className="nav-link text-dark"><BsInfoSquare /> Welcome</NavLink></li>
 						<li className="nav-item"><NavLink to="/createItem" className="nav-link text-dark"><BsArrowLeftRight /> Create Item</NavLink></li>
 						<li className="nav-item"><NavLink to="/createPage" className="nav-link text-dark"><MdCreate /> Create Page</NavLink></li>
 						<li className="nav-item"><NavLink to="/about" className="nav-link text-dark"><AiOutlineQuestionCircle /> About</NavLink></li>
 					</ul>
 
-					<div class="page bg-dark text-white p-3 ml-4">
+					<div className="page bg-dark text-white p-3 ml-4">
 						<Switch>
 							<Route path="/welcome" component={PageWelcome} />
 							<Route path="/createItem" component={PageCreateItem} />
@@ -37,7 +55,10 @@ class App extends Component {
 							<Route path="/about" component={PageAbout} />
 						</Switch>
 					</div>
-					<div className="ml-4 mt-2 text-primary">{this.state.location}</div>
+					<div className="ml-4 mt-2 text-dark">
+						<h3>Flashcards:</h3>
+						{flashcardsHtml}
+					</div>
 				</BrowserRouter>
 			</main>
 		);
